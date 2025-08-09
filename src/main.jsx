4c1b8c37773e82f0c597baa37149542632414e34
sejcore <sejinyoon@gmail.com>
2025-08-09 22:24:21 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,10 @@
-s
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
