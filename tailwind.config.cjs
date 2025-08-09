96df3a1fe488dc85d92e1ca8388dc630dc90a10a
sejcore <sejinyoon@gmail.com>
2025-08-09 22:33:39 +0900

    Update and rename tailwind.config.js to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     './index.html',
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
-      colors: {
-        gold: '#FFD54A',
-      },
+      colors: { gold: '#FFD54A' },
     },
   },
   plugins: [],
