bec9b4e6eaf3f937bfc3b1a8f6ee2b3aa3cee4b2
sejcore <sejinyoon@gmail.com>
2025-08-09 22:37:41 +0900

    Update tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './index.html',
-    './src/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {
-      colors: { gold: '#FFD54A' },
-    },
-  },
+  content: ['./index.html','./src/**/*.{js,jsx,ts,tsx}'],
+  theme: { extend: { colors: { gold: '#FFD54A' } } },
   plugins: [],
 };
